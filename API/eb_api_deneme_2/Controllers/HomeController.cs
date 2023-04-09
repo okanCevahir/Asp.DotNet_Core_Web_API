d7eff5cb6362018bd2665b8c1777f9b31271260d
Okan Cevahir <cevahir.okan1@gmail.com>
2023-04-10 02:58:46 +0300

    Bolum_1

---
+++ API/eb_api_deneme_2/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using eb_api_deneme_2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eb_api_deneme_2.Controllers
+{
+    [ApiController]
+    [Route("Home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+            var resut=  new ResponseModel() { HttpStatus=200, Message="Hello"};
+            return Ok(resut);
+        }
+    }
+}
---
+++ API/eb_api_deneme_2/Models/ResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace eb_api_deneme_2.Models
+{
+    public class ResponseModel
+    {
+        public int HttpStatus { get; set; }
+        public string Message { get; set; }
+    }
+}
