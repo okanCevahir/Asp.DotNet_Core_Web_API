165a8e0594e8915d06ff89813724ff3c12de07ce
Okan Cevahir <cevahir.okan1@gmail.com>
2023-04-11 01:37:12 +0300

    Bolum_2

---
+++ API/ProductApp/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProduct()
+        {
+            var products = new List<Product>()
+            {
+                new Product(){id=1,ProductName="mause"},
+                new Product(){id=2,ProductName="phone"},
+                new Product(){id=3,ProductName="computer"},
+            };
+            _logger.LogInformation("GetAllProdut action has been called");
+            _logger.LogWarning("Try");
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public IActionResult GetAllProduct([FromBody]Product product)
+        {
+            _logger.LogWarning("Creat get warning");
+            return StatusCode(201);
+        }
+    }
+}
